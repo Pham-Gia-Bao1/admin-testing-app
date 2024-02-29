40ce194677db3db60b0f8395f5d511ada72c4a50
PhamGiaBao <phamgiabao123abc@gmail.com>
2024-02-29 22:46:11 +0700

    start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [videos, setVideos] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/api/items');
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            setVideos(data);
+        } catch (error) {
+            console.error('Error fetching data:', error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Videos from API</h1>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <ul>
+                    {videos.map(video => (
+                        <li key={video.id}>
+                            <h2>{video.title}</h2>
+                            <p>{video.description}</p>
+                            <p>Duration: {video.duration} seconds</p>
+                            <p>Views: {video.view}</p>
+                            <p>Likes: {video.like_count}</p>
+                            <p>Dislikes: {video.dislike_count}</p>
+                            <iframe title={video.title} width="560" height="315" src={video.youtube_link} frameBorder="0" allowFullScreen></iframe>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 }
 
 export default App;
