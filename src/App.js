2475af50b19329a9618b31aad10b27025a8102e1
Anh Tuan Pham <tuanpham@everfit.io>
2024-02-29 23:17:29 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,50 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-    const [videos, setVideos] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // code của Tuan
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/api/items");
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch('http://localhost:8000/api/items');
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
 
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            const data = await response.json();
-            setVideos(data);
-        } catch (error) {
-            console.error('Error fetching data:', error.message);
-        } finally {
-            setLoading(false);
-        }
-    };
-    return (
-        <div>
-            <h1>Videos from API</h1>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <ul>
-                    {videos.map(video => (
-                        <li key={video.id}>
-                            <h2>{video.title}</h2>
-                            <p>{video.description}</p>
-                            <p>Duration: {video.duration} seconds</p>
-                            <p>Views: {video.view}</p>
-                            <p>Likes: {video.like_count}</p>
-                            <p>Dislikes: {video.dislike_count}</p>
-                            <iframe title={video.title} width="560" height="315" src={video.youtube_link} frameBorder="0" allowFullScreen></iframe>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+      const data = await response.json();
+      setVideos(data);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div>
+      <h1>Videos from API</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {videos.map((video) => (
+            <li key={video.id}>
+              <h2>{video.title}</h2>
+              <p>{video.description}</p>
+              <p>Duration: {video.duration} seconds</p>
+              <p>Views: {video.view}</p>
+              <p>Likes: {video.like_count}</p>
+              <p>Dislikes: {video.dislike_count}</p>
+              <iframe
+                title={video.title}
+                width="560"
+                height="315"
+                src={video.youtube_link}
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 export default App;
