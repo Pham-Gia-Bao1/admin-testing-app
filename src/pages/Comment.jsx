03d17161a13dbc81315bf1cf4738e24c64552795
thu <thu.vo25@student.passerellesnumeriques.org>
2024-05-21 22:29:41 +0700

    read comment

--- src/pages/Comment.jsx
+++ src/pages/Comment.jsx
@@ -1,5 +1,8 @@
 import Column from "../components/column";
 const Comment = () => {
-  return <Column apiEndpoint="https://jsonplaceholder.typicode.com/comments" />; // thay omponent khác vào
+  function onDelete(record){
+    console.log(record.id)
+  }
+  return <Column apiEndpoint="http://127.0.0.1:8000/api/admin/comments" onDelete={onDelete} />; // thay omponent khác vào
 };
 export default Comment;
